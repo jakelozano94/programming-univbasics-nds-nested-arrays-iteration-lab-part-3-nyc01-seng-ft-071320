53bb4168a1ac9c829110b4ca2bcb21a6ce263aae
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-06 05:19:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,8 +10,7 @@ end
 element_index += 1 
 end
 row_index += 1 
-end 
-print end_string
+end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
