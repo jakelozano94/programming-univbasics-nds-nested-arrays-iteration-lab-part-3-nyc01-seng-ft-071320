9b6464240cc3e8b2d64e7af1292c26343ead3492
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-06 05:16:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count
-  if src[row_index][element_index].class = String
+  if src[row_index][element_index].class == String
 end_string << src[row_index][element_index]
 end
 element_index += 1 
